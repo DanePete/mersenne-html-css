61d755b0999aa97773f5501248de639cd37daa0e
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-22 13:23:13 -0500

    added button that adds item to list as well as a button that clears out the list

--- events.js
+++ events.js
@@ -1,1 +1,21 @@
 console.log('got here');
+
+// The Incantation
+
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('so ready');
+
+  $('#addBtn').on('click', addToList);
+  $('#clearList').on('click', clearList);
+}
+
+function addToList() {
+  console.log('button got clicked!');
+  $('ul').append('<li>hello</li>');
+}
+
+function clearList() {
+  $('ul').empty();
+}
